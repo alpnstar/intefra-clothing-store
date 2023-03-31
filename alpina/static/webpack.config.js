d07120083dba59b90ba8db8dae62668585a9550e
Satiyadzhiev Daud Adjakavovich <alpn.ds@mail.ru>
2023-03-31 23:15:59 +0300

    commit

--- alpina/static/webpack.config.js
+++ alpina/static/webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
             {
                 test: /\.(c|sa|sc)ss$/i,
                 use: [
-                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                    MiniCssExtractPlugin.loader,
                     "css-loader",
                     {
                         loader: 'postcss-loader',
